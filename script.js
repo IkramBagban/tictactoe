00c7614e40eda21ec9fcea5a178712c20f4f62c5
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-27 14:48:16 +0530

    write style and more logic for game. inhanced computer thinking ability

---
+++ script.js
@@ -0,0 +1,151 @@
+let board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+
+let currentPlayer;
+let isComputerTurn;
+
+function startNewGame() {
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            board[row][col] = '';
+        }
+    }
+    currentPlayer = Math.random() < 0.5 ? 'X' : 'O';
+    isComputerTurn = currentPlayer === 'O'; 
+    updateTurnDisplay();
+    updateBoard();
+    if (isComputerTurn) {
+        setTimeout(makeComputerMove, 500); 
+    }
+}
+
+function makeMove(row, col) {
+    if (!isComputerTurn && board[row][col] === '') {
+        board[row][col] = currentPlayer;
+        handleTurn();
+    }
+}
+
+function handleTurn() {
+    updateBoard();
+    if (checkWinner(currentPlayer)) {
+        document.getElementById('playerTurn').innerText = 'Player ' + currentPlayer + ' Wins!';
+        alert('Player ' + currentPlayer + ' Wins!');
+        startNewGame();
+        return;
+    } else if (isBoardFull()) {
+        document.getElementById('playerTurn').innerText = 'Game is a Draw!';
+        alert('Game is a Draw!');
+        startNewGame();
+        return;
+    }
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    isComputerTurn = !isComputerTurn;
+    updateTurnDisplay();
+    if (isComputerTurn) {
+        setTimeout(makeComputerMove, 500);
+    }
+}
+
+function makeComputerMove() {
+    let moveMade = tryToWin('O'); 
+
+    if (!moveMade) {
+        moveMade = blockOpponentWin('X');
+    }
+
+    if (!moveMade) {
+        let availableMoves = [];
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                if (board[row][col] === '') {
+                    availableMoves.push({row, col});
+                }
+            }
+        }
+        if (availableMoves.length > 0) {
+            let randomMove = availableMoves[Math.floor(Math.random() * availableMoves.length)];
+            board[randomMove.row][randomMove.col] = currentPlayer;
+        }
+    }
+    handleTurn();
+}
+
+function tryToWin(player) {
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (board[row][col] === '') {
+                board[row][col] = player;
+                let winningMove = checkWinner(player);
+                board[row][col] = ''; 
+                if (winningMove) {
+                    board[row][col] = player; 
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+
+function blockOpponentWin(player) {
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (board[row][col] === '') {
+                board[row][col] = player; 
+                let winningMove = checkWinner(player);
+                board[row][col] = ''; 
+                if (winningMove) {
+                    board[row][col] = 'O'; 
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function updateBoard() {
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            let cell = document.getElementById('row' + row).children[col];
+            cell.innerText = board[row][col];
+        }
+    }
+}
+
+function checkWinner(player) {
+    for (let i = 0; i < 3; i++) {
+        if (board[i][0] === player && board[i][1] === player && board[i][2] === player ||
+            board[0][i] === player && board[1][i] === player && board[2][i] === player) {
+            return true;
+        }
+    }
+    if (board[0][0] === player && board[1][1] === player && board[2][2] === player ||
+        board[0][2] === player && board[1][1] === player && board[2][0] === player) {
+        return true;
+    }
+    return false;
+}
+
+function isBoardFull() {
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (board[row][col] === '') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function updateTurnDisplay() {
+    document.getElementById('playerTurn').innerText = 'Player: ' + (isComputerTurn ? 'Computer' : currentPlayer) + "'s Turn";
+}
+
+startNewGame(); 
+
